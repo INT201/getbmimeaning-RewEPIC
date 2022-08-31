698457bcaf46d280e414574d1c866f688d6c38a9
RewEPIC <78597357+RewEPIC@users.noreply.github.com>
2022-09-01 00:11:44 +0700

    Update getBMIMeaning.js

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,13 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+    return weight/(height*height)
 }
 function getBMIMeaning(weight, height) {
-  //code here
+    let bmi = calculateBMI(weight,height)
+    //BMI < 18.5 => Underweight
+    //BMI 18.5 -> 24.9 => Normal
+    //BMI > 25 => Overweight
+    return bmi > 25 ? "Overweight" : (bmi < 18.5 ? "Underweight" : "Normal weight")
 }
 module.exports = getBMIMeaning
